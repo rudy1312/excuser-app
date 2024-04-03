0d3128e6ffe337f8b34037a6c605a5cf2aa4056e
Rudraraj Kundu <94010970+rudy1312@users.noreply.github.com>
2024-04-04 04:46:59 +0530

    Wrote the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-export default function App() {
+function App() {
+
+    const buttonStyle="w-20 rounded-xl h-14 my-2 mx-1 p-1 bg-amber-600 text-gray-700"
+
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello worldS!
-        </h1>
-    )
+        <div className="App bg-amber-400 text-xl text-amber-700 w-screen h-screen flex justify-center">
+            <div>
+                <h1 className="text-4xl text-center">Generate an Excuse</h1>
+
+                <div className="Buttons flex justify-center items-center">
+
+                    <button className={buttonStyle}>
+                        Family
+                    </button>
+                    <button className={buttonStyle}>
+                        Party
+                    </button>
+                    <button className={buttonStyle}>
+                        Office
+                    </button>
+
+                </div>
+
+                <p className="text-center my-2 text-pink-600 mx-1">EXAMPLE EXCUSE</p>
+
+            </div>
+
+        </div>
+    );
 }
+
+export default App;
